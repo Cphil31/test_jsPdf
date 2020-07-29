11800020425a473c21fb4fa980785b19c2b46df7
Cphil31 <cecilephilippe31@gmail.com>
2020-07-29 10:12:35 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+    let boutton = document.querySelector('#valider');
+    console.log(boutton);
+    // Default export is a4 paper, portrait, using millimeters for units
+    var doc = new jsPDF();
+    
+    doc.text('Hello world!', 10, 10);
+    doc.save('a4.pdf');
+
+
+}
