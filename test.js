204c079477816741ee0809318c8ba19431c4805b
Cphil31 <cecilephilippe31@gmail.com>
2020-07-29 11:15:53 +0200

    test avec une fontion

--- test.js
+++ test.js
@@ -2,15 +2,36 @@ window.onload = function () {
 
     let boutton = document.querySelector('#valider');
     console.log(boutton);
+
+    // j'initialise la variable page 
+    let page = 1; 
+
+    //j'appelles ma fonction 
+    numerotation();
+
+    console.log(page);
+
+
+    //je cree ma fonction numerotation
+    function numerotation () {
+        page += 30;
+    }
+    
+    let resultat = `${page} pages`;
+    console.log(resultat);
+
+    // j'ajoutes au html
+    let pnum = document.querySelector('.num');
+    console.log(pnum);
+    pnum.innerHTML = resultat ;
+
     boutton.addEventListener("click",function(){
 
         // Default export is a4 paper, portrait, using millimeters for units
         var doc = new jsPDF();
         
-        doc.text('Hello world!', 10, 10);
-        doc.save('a4.pdf');
+        doc.text(`${resultat}`, 10, 10);
+        doc.save('new.pdf');
     });
-    let a = 0; 
-    a += 2;
-    console.log(b);
+
 }
