892d829105b1258fe18d06dd150b746db846330d
Cphil31 <cecilephilippe31@gmail.com>
2020-07-30 14:40:51 +0200

    titre avant boutton

--- inc/header.php
+++ inc/header.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="node_modules/jspdf/dist/jspdf.min.js"></script>
-    <script src="js/app.js"></script> 
+    <script src="js/app.js"></script>
+
 </head>
 <body>
--- js/app.js
+++ js/app.js
@@ -1,53 +1,67 @@
 window.onload = function () {
     
-    // je crée mon container 
-    let container = document.createElement("div");
-    container.setAttribute("class","container");
-    document.body.appendChild(container);
+    function structure (){
+        // je crée mon container 
+        let container = document.createElement("div");
+        container.setAttribute("class","container");
+        document.body.appendChild(container);
+  
 
-    // je crée ma row
-    let row = document.createElement("div");
-    row.setAttribute("class","row");
-    container.appendChild(row);
+        //je crée ma row pour mon titre 
+        let rowTitre = document.createElement("div");
+        rowTitre.setAttribute("class","row rowTitre");
+        container.appendChild(rowTitre);
     
-    // je crée ma colone 
-    let col = document.createElement("div");
-    col.setAttribute("class","col");
-    row.appendChild(col);
-    
-    
-    //je crée ma row pour mon titre 
-    let rowTitre = document.createElement("div");
-    rowTitre.setAttribute("class","row");
-    container.appendChild(rowTitre);
-    // je crée ma col pour mon titre
-    let colTitre = document.createElement("div");
-    colTitre.setAttribute("class","col");
-    rowTitre.appendChild(colTitre);
-    
-    
-    // je crée mon h1 titre 
-    let titre = document.createElement("h1");
-    titre.innerHTML = "Acceuil";
-    colTitre.appendChild(titre);
-    
-    
-    // je crée mon boutton
-    let boutton = document.createElement("button");
-    boutton.setAttribute("id","valider");
-    boutton.setAttribute("type","button");
-    boutton.setAttribute("class","btn btn-danger");
-    boutton.innerHTML="Valider";
-    col.appendChild(boutton);
+        // je crée ma col pour mon titre
+        let colTitre = document.createElement("div");
+        colTitre.setAttribute("class","col");
+        rowTitre.appendChild(colTitre);
+        
+        
+        // je crée mon h1 titre 
+        let titre = document.createElement("h1");
+        titre.innerHTML = "Acceuil";
+        colTitre.appendChild(titre);
 
+        // je crée ma row
+        let row = document.createElement("div");
+        row.setAttribute("class","row");
+        container.appendChild(row);
+        
+        // je crée ma colone 
+        let col = document.createElement("div");
+        col.setAttribute("class","col content");
+        row.appendChild(col);
+    }
+    
+    function createButton (){
 
-    boutton.addEventListener("click",function(){
+        let content = document.querySelector(".content");
+        
+        // je crée mon boutton
+        let boutton = document.createElement("button");
+        boutton.setAttribute("id","valider");
+        boutton.setAttribute("type","button");
+        boutton.setAttribute("class","btn btn-danger");
+        boutton.innerHTML="Valider";
+        content.appendChild(boutton);  
+    
+    }
+
+    structure();  
+    createButton();
+    let download = document.querySelector("#valider");
+    let titre = "Baby shark ";
+    let sousTitre = "Dessin animé ";
+    let ml = 60 ;
+    let mt = 0 ;
+    download.addEventListener("click",function(){
         // Default export is a4 paper, portrait, using millimeters for units
         var doc = new jsPDF();
-        doc.text(`hello`, 10, 10);
+        doc.text(`${titre}`, 60, 0);
+        doc.text(`${sousTitre}`,60,20)
+
         doc.save('new.pdf');
     });
-    /* 
-    */    
-   
+    
 };
